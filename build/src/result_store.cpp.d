046dad7f1fcf95c7a99b044e11e73aa030009413
John Smith <john.smithh@gmx.de>
2018-07-20 21:14:47 +0200

    ui

--- build/src/result_store.cpp.d
+++
@@ -1,6 +0,0 @@
-build/./src/result_store.cpp.o: src/result_store.cpp src/result_store.hpp \
- src/types.hpp
-
-src/result_store.hpp:
-
-src/types.hpp:
--- build/src/workload_distributor.cpp.d
+++
@@ -1,6 +0,0 @@
-build/./src/workload_distributor.cpp.o: src/workload_distributor.cpp \
- src/workload_distributor.hpp src/types.hpp
-
-src/workload_distributor.hpp:
-
-src/types.hpp:
